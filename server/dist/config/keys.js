164e0d6d596a0af0ba1f337f83b67e60e103ca36
Nurbek <rustamovnurbek02@gmail.com>
2024-08-22 18:47:37 +0500

    commit of 22.8 3.1

--- server/dist/config/keys.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WEB_APP_URL = exports.BOT_TOKEN = exports.JWT_SECRET = exports.PORT = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-exports.PORT = process.env.PORT;
-exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.BOT_TOKEN = process.env.TOKEN;
-exports.WEB_APP_URL = process.env.WEB_APP_URL;
--- server/dist/routes/auth.route.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_controller_1 = require("../controllers/auth.controller");
-exports.default = (0, express_1.Router)()
-    .post('/login', auth_controller_1.login);
